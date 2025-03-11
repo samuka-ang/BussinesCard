e79929ed8e92ed31c417b899cfd1f2b0e3e9aa7d
samuka-ang <samuelcgangulo@gmail.com>
2025-03-10 23:43:00 -0300

    of course

--- app/src/main/java/com/example/bussinescard/MainActivity.kt
+++ app/src/main/java/com/example/bussinescard/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.bussinescard
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.background
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material.icons.Icons
@@ -19,7 +18,6 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.ui.text.font.FontWeight
 
 @Composable
 fun BusinessCardScreen() {
